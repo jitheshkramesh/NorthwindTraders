fcd1e583d301bba2c66dcdd82c05570bbec985b9
JITHESH <jitheshkramesh@hotmail.com>
2019-06-09 13:03:38 +0400

    first commit

---
+++ Northwind.WebUI/ClientApp/src/app/customers/customers.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CustomersClient, CustomersListViewModel } from '../northwind-traders-api';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { BsModalService } from 'ngx-bootstrap/modal/bs-modal.service';
+import { CustomerDetailComponent } from '../customer-detail/customer-detail.component';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html'
+})
+export class CustomersComponent {
+
+  public vm: CustomersListViewModel = new CustomersListViewModel();
+  private bsModalRef: BsModalRef;
+
+  constructor(private client: CustomersClient, private modalService: BsModalService) {
+    client.getAll().subscribe(result => {
+      this.vm = result;
+    }, error => console.error(error));
+  }
+
+  public customerDetail(id: string) {
+    this.client.get(id).subscribe(result => {
+      const initialState = {
+        customer: result
+      };
+      this.bsModalRef = this.modalService.show(CustomerDetailComponent, {initialState});
+    }, error => console.error(error));
+  }
+}
